7f0459733d3a6d0408c495915a7d28c0ad758e22
dravikum <dravikum@ecegrid-thin7.ecn.purdue.edu>
2018-03-19 23:09:39 -0400

    Added basic skeleton for OpenMP

---
+++ OpenMP/src/dataStructures.cpp
@@ -0,0 +1,7 @@
+#include <dataStructures.hpp>
+
+void InitializeWQList(workQueueList wQList, int mapperThreads)
+{
+	// TODO
+	// For each mapperThreads create a new queue and add to the workQueueList
+}
