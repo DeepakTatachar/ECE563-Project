97b8d36c14a998caeec4761110600b2ab72a7dbb
Deepak Ravikumar <dravikum@scholar-fe00.rcac.purdue.edu>
2018-04-24 23:05:40 -0400

    Updates to utility

--- Utilities/outputParser.py
+++ Utilities/outputParser.py
@@ -20,19 +20,20 @@ def cleanAndWriteFile(inputFile, outputFile):
 
 	with open(inputFile) as fileHandle:
 		for line in fileHandle:
-
-			removedNodes = re.sub(r'Running for (.*) nodes (.*) reader (\w+) mapper threads and (\w+) data size\n', r'', line)
-			removedTime = re.sub(r'Time ', '', removedNodes)
+			removedTime = re.sub('Running for (\w) data size and rT (\w)\n', ' ',line)
+			#removedNodes = re.sub(r'Running for (.*) nodes (.*) reader (\w+) mapper threads and (\w+) data size\n', r' ', line)
+			#removedTime = re.sub(r'Time ', '', removedNodes)
 			removedNewLine = re.sub('\n', ' ', removedTime)
 		        buf = buf + removedNewLine
 
 			i = i + 1
-			v = i % 22
+			#v = i % 22
+			v = i % 16			
 			if(v == 0):
 				cleaned = cleaned + buf + '\n'
 				buf = ''
 
-	finalString = cleaned
+	finalString = cleaned + buf
 	outFileHandle = open(outputFile, 'w')
 	outFileHandle.write(finalString)
 	outFileHandle.close()
